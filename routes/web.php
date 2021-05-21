ab6eba7389ff3748afd469e26a02510eabd74659
Rahmatuldani <danirahmatul@gmail.com>
2021-05-21 08:09:11 +0700

    disnaker

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
